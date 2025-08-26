9ffc15193e8984a2c43ad4e79084e0820c5e82ef
Nimujaiswal <124816011+Nimujaiswal@users.noreply.github.com>
2025-08-26 19:49:14 +0530

    Update Impact.js

--- src/components/Impact.js
+++ src/components/Impact.js
@@ -5,7 +5,7 @@ const Impact = () => {
   return (
     <section className="impact">
       <div className="impact-container">
-        {/* Dark blue rounded card */}
+      
         <div className="impact-card">
           <h2 className="impact-title">Our Impact</h2>
           <p className="impact-sub">
@@ -14,28 +14,27 @@ const Impact = () => {
           </p>
 
           <div className="impact-grid">
-            {/* 1 */}
+      
             <div className="impact-item">
               <div className="impact-label">Engineering &amp; Technical DD</div>
               <div className="impact-value">14 WW+</div>
               <div className="impact-caption">Lorem ipsum Lorem &amp; ipsum</div>
             </div>
 
-            {/* 2 */}
+         
             <div className="impact-item">
               <div className="impact-label">Fundraise &amp; Advisory</div>
               <div className="impact-value">USD 1.2Mn+</div>
               <div className="impact-caption">Lorem ipsum Lorem &amp; ipsum</div>
             </div>
 
-            {/* 3 */}
             <div className="impact-item">
               <div className="impact-label">Procurement As A Service (PaaS)</div>
               <div className="impact-value">5 WW+</div>
               <div className="impact-caption">Lorem ipsum Lorem &amp; ipsum</div>
             </div>
 
-            {/* 4 */}
+     
             <div className="impact-item">
               <div className="impact-label">Asset Management &amp; O&amp;M</div>
               <div className="impact-value">5 WW+</div>
@@ -44,7 +43,6 @@ const Impact = () => {
           </div>
         </div>
 
-        {/* Light strip with uppercase italic line + logo placeholders */}
         <div className="impact-strip">
           <p className="impact-quote">
             LOREM IPSUM HAS BEEN THE INDUSTRY’S STANDARD DUMMY TEXT EVER SINCE THE 1500S.
